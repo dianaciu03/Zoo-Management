11905b6047c2c1db55c520883aa65f35d79709e2
Augustinas Digrys <digrys.augustinas@gmail.com>
2023-03-09 16:16:01 +0100

    Added classes for Employee/Task management

---
+++ ZooBazaar/ZooBazaarLogicLibrary/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooBazaarLogicLibrary
+{
+    public class Manager : Employee
+    {
+        public Manager(int ID, string FirstName, string LastName, GENDER PersonGender, string Address, string Password, string Email) : 
+            base(ID, FirstName, LastName, PersonGender, Address, Password, Email)
+        {
+
+        }
+        public string GetRole()
+        {
+            return this.GetType().Name;
+        }
+    }
+}
