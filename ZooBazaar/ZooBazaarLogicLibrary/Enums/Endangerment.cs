8c2f1d4d383be2329ef0540b411ef71edab18a04
Milan <m.rasche@student.fontys.nl>
2023-03-13 11:31:10 +0100

    Added a folder for enums

--- ZooBazaar/ZooBazaarLogicLibrary/Enums/Endangerment.cs
+++ ZooBazaar/ZooBazaarLogicLibrary/Enums/Endangerment.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ZooBazaarLogicLibrary
+namespace ZooBazaarLogicLibrary.Enums
 {
     public enum Endangerment
     {
