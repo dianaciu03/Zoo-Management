eca52ba5e6017b122835d733a7140ce9e7f55664
Milan <m.rasche@student.fontys.nl>
2023-03-15 14:36:42 +0100

    Finished off functionality for getting a list of all animals from the database. Also added functionality for adding new animals to the database and updating existing animals

--- ZooBazaar/DatabaseLogicLibrary/DatabaseAnimalHelper.cs
+++ ZooBazaar/DatabaseLogicLibrary/DatabaseAnimalHelper.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using ZooBazaarLogicLibrary;
-using ZooBazaarLogicLibrary.Enums;
 
 namespace DatabaseLogicLibrary
 {
@@ -13,7 +13,7 @@ namespace DatabaseLogicLibrary
     {
         ConnectionHelper connectionHelper = new ConnectionHelper();
 
-        public List<Animal> GetAllAnimals()
+        public List<Animal> GetAllAnimals() // Gets all animals in the animal database and returns them as a list of Animal Objects
         {
             List<Animal> animals = new List<Animal>();
 
@@ -21,23 +21,96 @@ namespace DatabaseLogicLibrary
             {
                 try { connection.Open(); }
                 catch (SqlException) { return animals; }
-                
-                using (SqlCommand query = new SqlCommand("SELECT * FROM Animals", connection))
-                {
-                    using (SqlDataReader reader = query.ExecuteReader())
-                    {
-                        if (!reader.Read()) 
-                        {
-                            connection.Close();
-                            return animals; 
-                        }
 
-                        Animal animal = new Animal(reader.GetInt32(reader.GetOrdinal("AnimalID")),
-                                                   reader.GetString(reader.GetOrdinal("Name")),
-                                                   )
+                SqlCommand query = new SqlCommand("SELECT * FROM Animals", connection);
+
+                using (SqlDataReader reader = query.ExecuteReader())
+                {
+                    if (!reader.Read()) 
+                    {
+                        reader.Close();
+                        connection.Close();
+                        return animals; 
                     }
+
+                    Animal animal = new Animal(reader.GetInt32(reader.GetOrdinal("AnimalID")),
+                                                reader.GetString(reader.GetOrdinal("Name")),
+                                                reader.GetString(reader.GetOrdinal("Gender")),
+                                                reader.GetString(reader.GetOrdinal("Species")),
+                                                reader.GetDateTime(reader.GetOrdinal("BirthDate")),
+                                                (ORIGINCONTINENT)Enum.Parse(typeof(ORIGINCONTINENT), reader.GetString(reader.GetOrdinal("Origin"))),
+                                                reader.GetString(reader.GetOrdinal("Description")),
+                                                (ENDANGERMENT)Enum.Parse(typeof(ENDANGERMENT), reader.GetString(reader.GetOrdinal("Endangerment"))),
+                                                reader.GetInt32(reader.GetOrdinal("Enclosure")));
+                    animals.Add(animal);
                 }
             }
+           return animals;
+        }
+
+        public void AddUpdateAnimal(Animal animal)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionHelper.ConnectionValue()))
+            {
+                try { connection.Open(); }
+                catch(SqlException) { return; }
+
+                SqlCommand existsInDatabase = new SqlCommand($"SELECT COUNT(*) FROM Animals WHERE AnimalID = {animal.Id}");
+                if( (int)existsInDatabase.ExecuteScalar() > 0)
+                {
+                    UpdateAnimal( animal, connection );
+                }
+                else
+                {
+                    AddNewAnimal( animal, connection );
+                }
+                connection.Close();
+            }
+        }
+
+        private void AddNewAnimal( Animal animal , SqlConnection connection)
+        {
+            using (SqlCommand command = new SqlCommand("INSERT INTO Animals" +
+                                                       "VALUES (@AnimalID,@Name,Gender,@Species,@BirthDate,@Origin,@Description,@Endangerment,@Enclosure)", connection))
+            {
+                command.Parameters.AddWithValue("@AnimalID", animal.Id);
+                command.Parameters.AddWithValue("@Name", animal.Name);
+                command.Parameters.AddWithValue("@Gender", animal.Gender);
+                command.Parameters.AddWithValue("@Speciecs", animal.Species);
+                command.Parameters.AddWithValue("@BirthDate", animal.Birthday);
+                command.Parameters.AddWithValue("@Origin", animal.OriginContinent.ToString());
+                command.Parameters.AddWithValue("@Description", animal.Description);
+                command.Parameters.AddWithValue("@Endangerment", animal.Endangerment.ToString());
+                command.Parameters.AddWithValue("@Enclosure", animal.Enclosure);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void UpdateAnimal( Animal animal, SqlConnection connection)
+        {
+            using (SqlCommand command = new SqlCommand("UPDATE Animals " +
+                                                       "SET AnimalID = @AmimalID, Name = @Name, Gender = @Gender, Species = @Species, BirthDate = @BirthDate," +
+                                                       "Origin = @Origin, Description = @Description, Endangerment = @Endangerment, Enclosure = @Enclosure) " +
+                                                       "WHERE AnimalID = @AnmimalID", connection))
+            {
+                command.Parameters.AddWithValue("@AnimalID", animal.Id);
+                command.Parameters.AddWithValue("@Name", animal.Name);
+                command.Parameters.AddWithValue("@Gender", animal.Gender);
+                command.Parameters.AddWithValue("@Speciecs", animal.Species);
+                command.Parameters.AddWithValue("@BirthDate", animal.Birthday);
+                command.Parameters.AddWithValue("@Origin", animal.OriginContinent.ToString());
+                command.Parameters.AddWithValue("@Description", animal.Description);
+                command.Parameters.AddWithValue("@Endangerment", animal.Endangerment.ToString());
+                command.Parameters.AddWithValue("@Enclosure", animal.Enclosure);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public List<Animal> SearchAnimals(string Species)
+        {
+
         }
     }
 }
