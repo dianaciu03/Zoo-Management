f4414f44875ebdfbf1e1bd1112dbe0cdcbdc099c
Milan Home Pc <520540@student.fontys.nl>
2023-04-06 15:32:19 +0200

    added employee dto. made employee database class work with employe dto. added 2 methods (search and get new id) to employee database class

---
+++ ZooBazaar/DatabaseLogicLibrary/EmployeeDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLogicLibrary
+{
+    public class EmployeeDTO
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PersonGender { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int HoursPerWeek { get; set; }
+        public string EmployeeType { get; set; }
+
+        public EmployeeDTO(int ID, string FirstName, string LastName, DateTime BirthDate, string PersonGender, string Address, string Phone, string Password, string Email, int HoursPerWeek, string employeeType)
+        {
+            this.ID = ID;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.BirthDate = BirthDate;
+            this.PersonGender = PersonGender;
+            this.Address = Address;
+            this.Phone = Phone;
+            this.Password = Password;
+            this.Email = Email;
+            this.HoursPerWeek = HoursPerWeek;
+            this.EmployeeType = employeeType;
+        }
+    }
+}
