9472f3ddccf632ce6da699f6f847d1d2472defce
Milan Home Pc <520540@student.fontys.nl>
2023-05-07 19:35:12 +0200

    User detials page saving and updating is now done

--- ZooBazaar/WebApp/Pages/User.cshtml.cs
+++ ZooBazaar/WebApp/Pages/User.cshtml.cs
@@ -24,15 +24,21 @@ namespace WebApp.Pages
             }
             Employee? loggedInEmployee = employeeManagement.GetEmployeeById(Convert.ToInt32(HttpContext.Session.GetString(sessionKeyId)));
 
+            ID = loggedInEmployee.ID;
             FirstName = loggedInEmployee.FirstName;
             LastName = loggedInEmployee.LastName;
             Email = loggedInEmployee.Email;
             Address = loggedInEmployee.Address;
-
+            BirthDate = loggedInEmployee.BirthDate;
+            PersonGender = loggedInEmployee.PersonGender;
+            Phone = loggedInEmployee.Phone;
+            Password = loggedInEmployee.Password;
 
             return null;
         }
 
+        [BindProperty]
+        public int ID { get; set; }
         [BindProperty]
         public string FirstName { get; set; }
         [BindProperty]
@@ -41,10 +47,34 @@ namespace WebApp.Pages
         public string Email { get; set; }
         [BindProperty]
         public string Address { get; set; }
+        [BindProperty]
+        public DateTime BirthDate { get; set; }
+        [BindProperty]
+        public string PersonGender { get; set; }
+        [BindProperty]
+        public string Phone { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        [BindProperty]
+        public ROLE Role { get; set; }
+
 
         public void OnPost()
         {
-            //update employee detials here.
+            Employee updateEmployee = new Employee();
+
+            updateEmployee.ID = ID;
+            updateEmployee.FirstName = FirstName;
+            updateEmployee.LastName = LastName;
+            updateEmployee.Email = Email;
+            updateEmployee.Address = Address;
+            updateEmployee.BirthDate = BirthDate;
+            updateEmployee.PersonGender = PersonGender;
+            updateEmployee.Phone = Phone;
+            updateEmployee.Password = Password;
+            updateEmployee.Role = Role;
+
+            employeeManagement.UpdateEmployeeInformation(updateEmployee);
         }
 
     }
