f8af81ee5f03d992f85f055f7149bac06828bfdd
Diana Ciuperca <dianav.ciuperca@gmail.com>
2023-05-29 18:55:27 +0200

    Created order class

---
+++ ZooBazaar/BusinessLogic/Tickets/TicketOrder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Tickets
+{
+    public class TicketOrder
+    {
+        private List<Ticket> tickets;
+        public int OrderId { get; set; }
+        public string DiscountCode { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public TicketOrder()
+        {
+            tickets = new List<Ticket>();
+        }
+
+        public TicketOrder(int id)
+        {
+            OrderId = id;
+            tickets = new List<Ticket>();
+        }
+
+        public void AddTicketToOrder(Ticket ticket)
+        {
+            tickets.Add(ticket);
+        }
+
+        public void CalculateTotalPrice()
+        {
+            decimal totalPrice = 0;
+            foreach(Ticket ticket in tickets)
+            {
+                totalPrice += ticket.Price;
+            }
+            TotalPrice = totalPrice;
+        }
+
+    }
+}
--- ZooBazaar/WebApp/Pages/TicketBooth.cshtml.cs
+++ ZooBazaar/WebApp/Pages/TicketBooth.cshtml.cs
@@ -5,8 +5,26 @@ namespace WebApp.Pages
 {
     public class TicketBoothModel : PageModel
     {
+        [BindProperty]
+        public int AdultTicket { get; set; }
+
+        [BindProperty]
+        public int KidTicket { get; set; }
+
+        [BindProperty]
+        public int ToddlerTicket { get; set; }
+
+        [BindProperty]
+        public string DiscountCode { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+
+            return Page();
         }
     }
 }
