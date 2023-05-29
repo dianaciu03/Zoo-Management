48ab129ba0abd0eaaa8953e77997582150efdeee
Diana Ciuperca <dianav.ciuperca@gmail.com>
2023-05-29 16:21:16 +0200

    created ticket class

---
+++ ZooBazaar/BusinessLogic/Tickets/Ticket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Tickets
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+
+        public string TicketType { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Barcode { get; set; }
+
+        public Ticket(int ticketId, string ticketType, decimal price, DateTime date, string barcode)
+        {
+            TicketId = ticketId;
+            TicketType = ticketType;
+            Price = price;
+            Date = date;
+            Barcode = barcode;
+        }
+
+        public Ticket(string ticketType, decimal price, DateTime date, string barcode)
+        {
+            TicketType = ticketType;
+            Price = price;
+            Date = date;
+            Barcode = barcode;
+        }
+
+        public Ticket()
+        {
+
+        }
+    }
+}
