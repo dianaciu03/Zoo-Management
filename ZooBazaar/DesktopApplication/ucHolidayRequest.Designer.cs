1533f6d81a05641a43d780294f791783387544af
Alexandru Daba <alexdaba89@gmail.com>
2023-06-12 16:06:55 +0200

    Finished employee history,(AUGUSTINAS HELP HERE ty) working on Holiday.

---
+++ ZooBazaar/DesktopApplication/ucHolidayRequest.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace DesktopApplication
+{
+    partial class ucHolidayRequest
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxRequestHoliday = new System.Windows.Forms.GroupBox();
+            this.tbRole = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.btnCancelContractEdit = new System.Windows.Forms.Button();
+            this.btnSaveHoliday = new System.Windows.Forms.Button();
+            this.dateTimePickerEndDate = new System.Windows.Forms.DateTimePicker();
+            this.labelEndDate = new System.Windows.Forms.Label();
+            this.dateTimePickerStartDate = new System.Windows.Forms.DateTimePicker();
+            this.labelStartDate = new System.Windows.Forms.Label();
+            this.labelfirstName = new System.Windows.Forms.Label();
+            this.groupBoxRequestHoliday.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxRequestHoliday
+            // 
+            this.groupBoxRequestHoliday.Controls.Add(this.tbRole);
+            this.groupBoxRequestHoliday.Controls.Add(this.tbLastName);
+            this.groupBoxRequestHoliday.Controls.Add(this.tbFirstName);
+            this.groupBoxRequestHoliday.Controls.Add(this.label2);
+            this.groupBoxRequestHoliday.Controls.Add(this.labelLastName);
+            this.groupBoxRequestHoliday.Controls.Add(this.btnCancelContractEdit);
+            this.groupBoxRequestHoliday.Controls.Add(this.btnSaveHoliday);
+            this.groupBoxRequestHoliday.Controls.Add(this.dateTimePickerEndDate);
+            this.groupBoxRequestHoliday.Controls.Add(this.labelEndDate);
+            this.groupBoxRequestHoliday.Controls.Add(this.dateTimePickerStartDate);
+            this.groupBoxRequestHoliday.Controls.Add(this.labelStartDate);
+            this.groupBoxRequestHoliday.Controls.Add(this.labelfirstName);
+            this.groupBoxRequestHoliday.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBoxRequestHoliday.Location = new System.Drawing.Point(9, 20);
+            this.groupBoxRequestHoliday.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.groupBoxRequestHoliday.Name = "groupBoxRequestHoliday";
+            this.groupBoxRequestHoliday.Padding = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.groupBoxRequestHoliday.Size = new System.Drawing.Size(550, 502);
+            this.groupBoxRequestHoliday.TabIndex = 37;
+            this.groupBoxRequestHoliday.TabStop = false;
+            this.groupBoxRequestHoliday.Text = "Approve holiday";
+            // 
+            // tbRole
+            // 
+            this.tbRole.Enabled = false;
+            this.tbRole.Location = new System.Drawing.Point(164, 122);
+            this.tbRole.Name = "tbRole";
+            this.tbRole.Size = new System.Drawing.Size(165, 32);
+            this.tbRole.TabIndex = 57;
+            this.tbRole.TextChanged += new System.EventHandler(this.tbRole_TextChanged);
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Enabled = false;
+            this.tbLastName.Location = new System.Drawing.Point(164, 84);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(165, 32);
+            this.tbLastName.TabIndex = 56;
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Enabled = false;
+            this.tbFirstName.Location = new System.Drawing.Point(164, 51);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(165, 32);
+            this.tbFirstName.TabIndex = 55;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 128);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 26);
+            this.label2.TabIndex = 54;
+            this.label2.Text = "Role:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(17, 84);
+            this.labelLastName.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(120, 26);
+            this.labelLastName.TabIndex = 53;
+            this.labelLastName.Text = "Last name:";
+            // 
+            // btnCancelContractEdit
+            // 
+            this.btnCancelContractEdit.Location = new System.Drawing.Point(11, 254);
+            this.btnCancelContractEdit.Name = "btnCancelContractEdit";
+            this.btnCancelContractEdit.Size = new System.Drawing.Size(126, 23);
+            this.btnCancelContractEdit.TabIndex = 52;
+            this.btnCancelContractEdit.Text = "Cancel";
+            this.btnCancelContractEdit.UseVisualStyleBackColor = true;
+            this.btnCancelContractEdit.Click += new System.EventHandler(this.btnCancelContractEdit_Click);
+            // 
+            // btnSaveHoliday
+            // 
+            this.btnSaveHoliday.Location = new System.Drawing.Point(190, 256);
+            this.btnSaveHoliday.Name = "btnSaveHoliday";
+            this.btnSaveHoliday.Size = new System.Drawing.Size(122, 21);
+            this.btnSaveHoliday.TabIndex = 51;
+            this.btnSaveHoliday.Text = "Save";
+            this.btnSaveHoliday.UseVisualStyleBackColor = true;
+            this.btnSaveHoliday.Click += new System.EventHandler(this.btnSaveHoliday_Click);
+            // 
+            // dateTimePickerEndDate
+            // 
+            this.dateTimePickerEndDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePickerEndDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePickerEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerEndDate.Location = new System.Drawing.Point(165, 225);
+            this.dateTimePickerEndDate.Margin = new System.Windows.Forms.Padding(6);
+            this.dateTimePickerEndDate.Name = "dateTimePickerEndDate";
+            this.dateTimePickerEndDate.Size = new System.Drawing.Size(147, 32);
+            this.dateTimePickerEndDate.TabIndex = 30;
+            // 
+            // labelEndDate
+            // 
+            this.labelEndDate.AutoSize = true;
+            this.labelEndDate.Location = new System.Drawing.Point(17, 225);
+            this.labelEndDate.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.labelEndDate.Name = "labelEndDate";
+            this.labelEndDate.Size = new System.Drawing.Size(105, 26);
+            this.labelEndDate.TabIndex = 31;
+            this.labelEndDate.Text = "End date:";
+            // 
+            // dateTimePickerStartDate
+            // 
+            this.dateTimePickerStartDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePickerStartDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePickerStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerStartDate.Location = new System.Drawing.Point(164, 171);
+            this.dateTimePickerStartDate.Margin = new System.Windows.Forms.Padding(6);
+            this.dateTimePickerStartDate.Name = "dateTimePickerStartDate";
+            this.dateTimePickerStartDate.Size = new System.Drawing.Size(148, 32);
+            this.dateTimePickerStartDate.TabIndex = 28;
+            // 
+            // labelStartDate
+            // 
+            this.labelStartDate.AutoSize = true;
+            this.labelStartDate.Location = new System.Drawing.Point(17, 171);
+            this.labelStartDate.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.labelStartDate.Name = "labelStartDate";
+            this.labelStartDate.Size = new System.Drawing.Size(112, 26);
+            this.labelStartDate.TabIndex = 29;
+            this.labelStartDate.Text = "Start date:";
+            // 
+            // labelfirstName
+            // 
+            this.labelfirstName.AutoSize = true;
+            this.labelfirstName.Location = new System.Drawing.Point(16, 51);
+            this.labelfirstName.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.labelfirstName.Name = "labelfirstName";
+            this.labelfirstName.Size = new System.Drawing.Size(121, 26);
+            this.labelfirstName.TabIndex = 23;
+            this.labelfirstName.Text = "First name:";
+            // 
+            // ucHolidayRequest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBoxRequestHoliday);
+            this.Name = "ucHolidayRequest";
+            this.Size = new System.Drawing.Size(569, 510);
+            this.groupBoxRequestHoliday.ResumeLayout(false);
+            this.groupBoxRequestHoliday.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBoxRequestHoliday;
+        private TextBox tbRole;
+        private TextBox tbLastName;
+        private TextBox tbFirstName;
+        private Label label2;
+        private Label labelLastName;
+        private Button btnCancelContractEdit;
+        private Button btnSaveHoliday;
+        private DateTimePicker dateTimePickerEndDate;
+        private Label labelEndDate;
+        private DateTimePicker dateTimePickerStartDate;
+        private Label labelStartDate;
+        private Label labelfirstName;
+    }
+}
---
+++ ZooBazaar/DesktopApplication/ucHolidayRequest.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApplication
+{
+    public partial class ucHolidayRequest : UserControl
+    {
+        Employee employee;
+        public ucHolidayRequest(Employee selectedEmployee)
+        {
+            //IDK HOW TO FIX VISUAL BUG FOR THIS UC WHEN IT'S DISPLAYED I LITERALLY COPY PASTED ALL OF IT WTFFFFFFF
+            InitializeComponent();
+            employee = selectedEmployee;
+            tbFirstName.Text = employee.FirstName;
+            tbLastName.Text = employee.LastName;
+            tbRole.Text = employee.Role.ToString();
+        }
+
+        private void btnCancelContractEdit_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnSaveHoliday_Click(object sender, EventArgs e)
+        {
+            if(dateTimePickerStartDate.Value > dateTimePickerEndDate.Value)
+            {
+                MessageBox.Show("Please make sure the start date is before the end date!");
+                return;
+            }
+            //REVIEW WITH AUGUSTINAS HOW TO IMPLEMENT IN DB, idk how the shifts work and I dont wanna break it
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbRole_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
