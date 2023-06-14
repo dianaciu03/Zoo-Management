8d0f3ae82e1f01fd428464f96b01a24fd609fb65
Diana Ciuperca <dianav.ciuperca@gmail.com>
2023-06-14 14:28:38 +0200

    UHHHH TEST

--- ZooBazaar/WebApp/Pages/OrderOverview.cshtml.cs
+++ ZooBazaar/WebApp/Pages/OrderOverview.cshtml.cs
@@ -11,6 +11,12 @@ namespace WebApp.Pages
         [BindProperty]
         public TicketOrder Order { get; set; }
 
+        [BindProperty]
+        public string DiscountCode { get; set; }
+
+        [BindProperty]
+        public decimal TotalPriceAfterDiscount { get; set; }
+
         public void OnGet()
         {
             if (TempData.ContainsKey("Order"))
@@ -18,12 +24,13 @@ namespace WebApp.Pages
                 TicketOrder order = JsonSerializer.Deserialize<TicketOrder>(TempData["Order"].ToString())!;
                 order.CalculateTotalPrice();
                 Order = order;
-
             }
         }
 
         public IActionResult OnPost()
         {
+            TicketManagement tm = new TicketManagement();
+            TicketOrder newOrder = tm.AddOrder(Order);
             //SET THE NEW PRICE
             //ADD THE TICKETS TO DATABASE
             //ADD ORDER TO DATABASE
@@ -31,8 +38,13 @@ namespace WebApp.Pages
             //GET TICKETS FROM DATABASE
             //CREATE NEW ORDER OBJECT
             //ADD TICKETS TO NEW ORDER OBJECT (this should have the new total price, ticket barcodes, valid date etc)
-            TempData["Order"] = JsonSerializer.Serialize(Order);
+            TempData["Order"] = JsonSerializer.Serialize(newOrder);
             return RedirectToPage("ConfirmedOrder");
         }
+
+        //public IActionResult OnPostValidateOrder()
+        //{
+            
+        //}
     }
 }
