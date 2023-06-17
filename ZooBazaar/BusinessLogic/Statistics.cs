22f175d35de1f822390c8314bf1aeb3f88f82538
Milan Rasche <i520540@fhict.nl>
2023-06-17 16:54:00 +0200

    added statistics not tested

---
+++ ZooBazaar/BusinessLogic/Statistics.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Statistics
+    {
+        StatisticsRepository StatisticsRepository;
+
+        public Statistics() 
+        {
+            StatisticsRepository = new StatisticsRepository();
+        }
+
+        public int TotalEmployees()
+        {
+            return StatisticsRepository.TotalEmployees();
+        }
+
+        public int TotalAnimals()
+        {
+            return StatisticsRepository.TotalAnimals();
+        }
+
+        public int TotalSpecies()
+        {
+            return StatisticsRepository.TotalSpecies();
+        }
+
+        public int CurrentVisitors()
+        {
+            return StatisticsRepository.CurrentVisitors();
+        }
+
+        public int TotalVisitorsOnGivenDay(DateTime date)
+        {
+            return StatisticsRepository.TotalVisitorsOnGivenDay(date);
+        }
+    }
+}
---
+++ ZooBazaar/DataAccess/StatisticsRepository.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class StatisticsRepository
+    {
+        ConnectionHelper connectionHelper = new ConnectionHelper();
+
+        public int TotalEmployees()
+        {
+            SqlConnection connection = new SqlConnection(connectionHelper.ConnectionValue());
+            using (connection)
+            {
+                int employeeCount = 0;
+                SqlCommand query = new SqlCommand("SELECT COUNT(EmployeeID) FROM Employees", connection);
+
+                try
+                {
+                    connection.Open();
+                    employeeCount = (int)query.ExecuteScalar();
+                }
+                catch (SqlException) { }
+                finally
+                {
+                    connection.Close();
+                }
+                return employeeCount;
+            }
+        }
+
+        public int TotalAnimals()
+        {
+            SqlConnection connection = new SqlConnection(connectionHelper.ConnectionValue());
+            using (connection)
+            {
+                int employeeCount = 0;
+                SqlCommand query = new SqlCommand("SELECT COUNT(AnimalID) FROM Animals", connection);
+
+                try
+                {
+                    connection.Open();
+                    employeeCount = (int)query.ExecuteScalar();
+                }
+                catch (SqlException) { }
+                finally
+                {
+                    connection.Close();
+                }
+                return employeeCount;
+            }
+        }
+
+        public int TotalSpecies()
+        {
+            SqlConnection connection = new SqlConnection(connectionHelper.ConnectionValue());
+            using (connection)
+            {
+                int employeeCount = 0;
+                SqlCommand query = new SqlCommand("SELECT COUNT(DISTINCT Species) FROM Animals", connection);
+
+                try
+                {
+                    connection.Open();
+                    employeeCount = (int)query.ExecuteScalar();
+                }
+                catch (SqlException) { }
+                finally
+                {
+                    connection.Close();
+                }
+                return employeeCount;
+            }
+        }
+
+        public int CurrentVisitors()
+        {
+            SqlConnection connection = new SqlConnection(connectionHelper.ConnectionValue());
+            using (connection)
+            {
+                int employeeCount = 0;
+                SqlCommand query = new SqlCommand("SELECT Count(BarcodeString) FROM sales " +
+                    "WHERE CONVERT(DATETIME, FLOOR(CONVERT(FLOAT, salesDate))) = @Date " +
+                    "AND Checkout IS NULL", connection);
+
+                query.Parameters.AddWithValue("@Date", DateTime.Today);
+
+                try
+                {
+                    connection.Open();
+                    employeeCount = (int)query.ExecuteScalar();
+                }
+                catch (SqlException) { }
+                finally
+                {
+                    connection.Close();
+                }
+                return employeeCount;
+            }
+        }
+
+        public int TotalVisitorsOnGivenDay(DateTime date)
+        {
+            SqlConnection connection = new SqlConnection(connectionHelper.ConnectionValue());
+            using (connection)
+            {
+                int employeeCount = 0;
+                SqlCommand query = new SqlCommand("SELECT Count(BarcodeString) FROM sales " +
+                    "WHERE CONVERT(DATETIME, FLOOR(CONVERT(FLOAT, salesDate))) = @Date ", connection);
+
+                query.Parameters.AddWithValue("@Date", date.Date);
+
+                try
+                {
+                    connection.Open();
+                    employeeCount = (int)query.ExecuteScalar();
+                }
+                catch (SqlException) { }
+                finally
+                {
+                    connection.Close();
+                }
+                return employeeCount;
+            }
+        }
+    }
+}
