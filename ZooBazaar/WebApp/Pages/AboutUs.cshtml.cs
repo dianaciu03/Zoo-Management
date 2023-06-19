4b97aad4ed0df688695970335335ff8840d990a6
Diana Ciuperca <dianav.ciuperca@gmail.com>
2023-06-19 11:15:52 +0200

    Statistics in making for web

--- ZooBazaar/WebApp/Pages/AboutUs.cshtml.cs
+++ ZooBazaar/WebApp/Pages/AboutUs.cshtml.cs
@@ -1,12 +1,38 @@
+using BusinessLogic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Pages
 {
     public class AboutUsModel : PageModel
     {
+        Statistics statistics = new Statistics();
+
+        [BindProperty]
+        public Statistics Stats { get; set; }
         public void OnGet()
         {
+        }
+
+        public int GetTotalAnimals()
+        {
+            return statistics.TotalAnimals();
+        }
+
+        public int GetTotalSpecies()
+        {
+            return statistics.TotalSpecies();
+        }
+
+        public int GetCurrentVisitors()
+        {
+            return statistics.CurrentVisitors();
+        }
+
+        public int GetTotalEmployees()
+        {
+            return statistics.TotalEmployees();
         }
     }
 }
