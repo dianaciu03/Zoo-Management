f464d9f2c41c2545a192553c44ca91a726adb129
Augustinas Digrys <digrys.augustinas@gmail.com>
2023-06-21 14:54:40 +0200

    Redirect to shifts correct path

--- ZooBazaar/WebApp/Pages/TaskDetails.cshtml.cs
+++ ZooBazaar/WebApp/Pages/TaskDetails.cshtml.cs
@@ -32,7 +32,7 @@ namespace WebApp.Pages
             int taskId = Convert.ToInt32(Request.Form["finishedTask"]);
             Task = taskManagement.GetTask(taskId);
             taskManagement.CompleteTask(Task);
-            return RedirectToPage("Shifts");
+            return RedirectToPage("ShiftSchedule");
         }
         [BindProperty]
         public ZooTask Task { get; set; }
